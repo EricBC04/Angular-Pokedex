053806922b76f3d66371c4aa9852208c1ab66290
Eric <eric.coraini@gmail.com>
2023-05-02 10:48:53 -0300

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ImageComponent } from './image/image.component';
+import { ButtonComponent } from './button/button.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ImageComponent, ButtonComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NgbModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pokedex.ts
@@ -0,0 +1,30 @@
+export interface Pokedex {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  abilities: [
+    {
+      ability: {
+        name: string;
+      };
+      slot: number;
+    }
+  ];
+  types: [
+    {
+      slot: 1;
+      type: {
+        name: string;
+      };
+    }
+  ];
+  weaknesses: [
+    {
+      slot: 2;
+      weaknesses: {
+        name: string;
+      }
+    }
+  ]
+}
